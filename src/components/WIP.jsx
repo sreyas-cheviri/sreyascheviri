d9a2d9f5161280a1ce2da53bf4293b8470812cdb
sreyascheviri <sreyascheviri@gmail.com>
2024-09-02 21:03:07 +0530

    ssdfsc

--- src/components/WIP.jsx
+++ src/components/WIP.jsx
@@ -4,7 +4,7 @@ import styles from "./WIP.module.css";
 export default function WIP() {
   return (
     <div className={styles.fullbox}>
-      <p className={styles.para}>***** WORK IN PROGRESS ***** WORK IN PROGRESS ***** WORK IN PROGRESS ***** WORK IN PROGRESS *****</p>
+      <p className={styles.para}>***** WORK IN PROGRESS ***** </p>
     </div>
   )
 }
